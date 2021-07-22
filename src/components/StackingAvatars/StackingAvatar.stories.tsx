f33830c137c237e6536f00179561aa5437ea7956
Satvik Verma <78793384+Satvik001@users.noreply.github.com>
2021-07-22 17:57:09 +0530

    StackingAvatar files made

---
+++ src/components/StackingAvatars/StackingAvatar.stories.tsx
@@ -0,0 +1,16 @@
+import StackingAvatar from "./StackingAvatar";
+export default{
+  title:"StackingAvatar",
+  component: StackingAvatar,
+}
+export const Main = (args: any) => <StackingAvatar {...args}></StackingAvatar>;
+Main.args = {
+     avatarArray: [
+    "https://image.flaticon.com/icons/png/512/147/147144.png",
+    "https://image.flaticon.com/icons/png/512/194/194938.png",
+    "https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScPuSSp3dmio9nEwA8FEAppD_ZzC7j7pCaDKYjiG1thpF4AyTQ-CvOonekXg3JdRl-_Kk&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvke8Pr8T6xz52yM8v0ieg0oQy9L9SwfkO4hy4IKoRpxyQBKSGUWto7sWmzj9YYgm1VzU&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTea6mpJZODj13Lvutndl6PgvULvVC3tPcreM4doidd7vHImnTOeK0HkfZIrFGeHuN_aJc&usqp=CAU",
+  ],
+};
